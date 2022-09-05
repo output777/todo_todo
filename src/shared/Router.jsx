c548bdda2ab8a01831b5e825c08e8e42f0b57bf9
oh <outputor777@gmail.com>
2022-09-05 10:59:03 +0900

    임시저장

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from '../pages/MainPage';
+import LoginPage from '../pages/LoginPage';
+import PlannerPage from '../pages/PlannerPage';
+import StatisticsPage from '../pages/StatisticsPage';
+import MyPage from '../pages/MyPage';
+
 
 const Router = () => {
   return (
-    <div>Router</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/main' element={<MainPage />} />
+        <Route path='/my' element={<MyPage />} />
+        <Route path='/planner' element={<PlannerPage />} />
+        <Route path='/statistics' element={<StatisticsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
