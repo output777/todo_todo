33136f0457239c21a1f4a4ded15a150b045bedc2
hyeonuk-choo <108967896+hyeonuk-choo@users.noreply.github.com>
2022-09-29 21:16:33 +0900

    [Chore] 마이페이지 이슈 처리

--- src/components/my/ProfilePhotos.jsx
+++ src/components/my/ProfilePhotos.jsx
@@ -21,35 +21,32 @@ const ProfilePhotos = () => {
   const { images } = useSelector((state) => state.my);
   console.log("images", images);
 
-  console.log(selectImgId);
-
   // 1
   const onClickFullScreenImgsHandler = (e) => {
     const { id } = e.target;
-    console.log("e.target.id", id);
+
     selectImgFunc(id);
   };
 
   // 2
   const selectImgFunc = (id) => {
     const data = images.filter((data) => data.id === Number(id));
-
     setSelectImgId(id);
     setSelectImg(data[0].imageUrl);
     setFullScreen(true);
   };
 
   const onClickPrevHandler = (id) => {
-    let a = images.find((data) => data.id === id);
-    const prevData = images[images.indexOf(a) - 1];
-    console.log(prevData);
+    let temp = images.find((data) => data.id === Number(id));
+    const prevData = images[images.indexOf(temp) - 1];
     setSelectImgId(prevData.id);
     setSelectImg(prevData.imageUrl);
   };
 
   const onClickNextHandler = (id) => {
-    let a = images.find((data) => data.id === id);
-    const nextData = images[images.indexOf(a) + 1];
+    let temp = images.find((data) => data.id === Number(id));
+
+    const nextData = images[images.indexOf(temp) + 1];
     setSelectImgId(nextData.id);
     setSelectImg(nextData.imageUrl);
   };
@@ -73,29 +70,28 @@ const ProfilePhotos = () => {
   };
 
   const nickname = localStorage.getItem("nickname");
-  // console.log("nickname", nickname);
 
   useEffect(() => {
     dispatch(__getImages(nickname));
   }, [dispatch]);
-  // console.log("selectImg", selectImg);
+
   return (
     <>
       <StContainer>
         {images.errorMessage === undefined &&
           images.map((data) => (
             <StImg key={data.id} onClick={onClickFullScreenImgsHandler}>
-              <img id={data.id} src={data.imageUrl} alt='boast' />
+              <img id={data.id} src={data.imageUrl} alt="boast" />
             </StImg>
           ))}
       </StContainer>
       {fullScreen ? (
         <StFullScreen>
-          <div className='header'>
-            <div className='cancelBox' onClick={onClickFullScreenCloseHandler}>
-              <img src={cancelSvg} alt='cancelBtn' />
+          <div className="header">
+            <div className="cancelBox" onClick={onClickFullScreenCloseHandler}>
+              <img src={cancelSvg} alt="cancelBtn" />
             </div>
-            <div className='imgCount'>
+            <div className="imgCount">
               {images && (
                 <span>
                   {images.indexOf(
@@ -105,39 +101,39 @@ const ProfilePhotos = () => {
                 </span>
               )}
             </div>
-            <div className='optionBox'>
+            <div className="optionBox">
               <img
                 src={wastebasketSvg}
-                alt='optionBtn'
+                alt="optionBtn"
                 onClick={onClicOptionModalOpenHandler}
               />
             </div>
           </div>
-          <div className='StSliderBoxParent'>
+          <div className="StSliderBoxParent">
             <StSliderBox>
-              <div className='imgBox' key={selectImgId}>
+              <div className="imgBox" key={selectImgId}>
                 {images.indexOf(
                   images.find((data) => data.id == selectImgId)
                 ) == 0 ? null : (
                   <button
-                    className='prev'
+                    className="prev"
                     onClick={() => onClickPrevHandler(selectImgId)}
                   >
-                    <div className='prevArrow'></div>
+                    <div className="prevArrow"></div>
                   </button>
                 )}
 
-                <img src={selectImg} alt='img' id={selectImgId} />
+                <img src={selectImg} alt="img" id={selectImgId} />
 
                 {images.indexOf(
                   images.find((data) => data.id == selectImgId)
                 ) ==
-                  images.length - 1 ? null : (
+                images.length - 1 ? null : (
                   <button
-                    className='next'
+                    className="next"
                     onClick={() => onClickNextHandler(selectImgId)}
                   >
-                    <div className='nextArrow'></div>
+                    <div className="nextArrow"></div>
                   </button>
                 )}
               </div>
@@ -154,21 +150,21 @@ const ProfilePhotos = () => {
             closable={true}
             maskClosable={true}
             onClose={closeModal}
-            radius='48px'
-            top='40%'
-            width='90%'
-            height='230px'
-            backgroundcolor='#46464624'
+            radius="48px"
+            top="40%"
+            width="90%"
+            height="230px"
+            backgroundcolor="#46464624"
           >
             <StModalTop>
-              <div className='title'>사진을 삭제하시겠습니까?</div>
+              <div className="title">사진을 삭제하시겠습니까?</div>
               <div>삭제하면 다시 불러올 수 없습니다.</div>
             </StModalTop>
             <StModalBottom>
-              <div className='cancel' onClick={closeModal}>
+              <div className="cancel" onClick={closeModal}>
                 취소
               </div>
-              <div className='confirm' onClick={onClickDeleteImgHandler}>
+              <div className="confirm" onClick={onClickDeleteImgHandler}>
                 확인
               </div>
             </StModalBottom>
